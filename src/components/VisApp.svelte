ee7a4e1274cdf6e3ea1ee5df10691c2987367aa4
MarlonPlease <44271128+MarlonPlease@users.noreply.github.com>
2024-03-01 20:59:43 -0800

    Update VisApp.svelte

--- src/components/VisApp.svelte
+++ src/components/VisApp.svelte
@@ -25,81 +25,13 @@
     // Assuming cleanData is your array of traffic collision incidents
     const filteredData = cleanData.filter(item => {
       const year = new Date(item['Date Occurred']).getFullYear();
-      return item['Victim Descent'] === 'H' && year === 2010;
-    });
-
-    // Extracting location coordinates of filtered incidents
-    const locationCoordinates = filteredData.map(item => {
-      const [longitude, latitude] = item['Location'].match(/\(([^)]+)\)/)[1].split(',').map(Number);
-      return [latitude, longitude]; // Reverse the order of coordinates
-    });
-
-    // Shuffle locationCoordinates array
-    const shuffledCoordinates = locationCoordinates.sort(() => Math.random() - 0.5);
-
-    // Take the first 100 coordinates
-    randomCoordinates = shuffledCoordinates.slice(0, 2000);
-
-    // Output random coordinates to the console
-    console.log(randomCoordinates);
-
-    // Initialize Mapbox GL map
-    mapboxgl.accessToken = "pk.eyJ1IjoibWdhcmF5IiwiYSI6ImNsc2ZpZXZ4aTFsdzAycXBkOWpqenZyeDIifQ._PjOouLcBA5ow4qkKjQaQw";
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-118.2437, 34.0522], // Los Angeles coordinates
-      zoom: 10, // Adjust the zoom level as needed
-      scrollZoom: false // Disable scroll zoom
-    });
-
-    map.on('style.load', () => {
-      // Remove existing circle layers (if any)
-      const existingLayers = map.getStyle().layers;
-      existingLayers.forEach(layer => {
-        if (layer.id.startsWith('circle-')) {
-          map.removeLayer(layer.id);
-        }
-      });
-
-      // Add circles for each random location coordinate
-      randomCoordinates.forEach(coord => {
-        // Check if the layer already exists before adding it
-        const layerId = 'circle-' + coord.join('-');
-        if (!map.getLayer(layerId)) {
-          map.addLayer({
-            'id': layerId, // Unique layer ID
-            'type': 'circle',
-            'source': {
-              'type': 'geojson',
-              'data': {
-                'type': 'Feature',
-                'geometry': {
-                  'type': 'Point',
-                  'coordinates': coord
-                }
-              }
-            },
-            'paint': {
-              'circle-color': 'blue', // Circle color
-              'circle-radius': 5, // Circle radius (adjust as needed)
-              'circle-opacity': 0.5 // Circle opacity
-            }
-          });
-        }
-      });
-
-
+      return year === 2010;
     });
   });
 </script>
 
 <style>
-  #map {
-    width: 100%;
-    height: 450px; /* Adjust the height as needed */
-    overflow: hidden; /* Hide scrollbars */
-  }
+
 </style>
 
-<div id="map"></div>
+
