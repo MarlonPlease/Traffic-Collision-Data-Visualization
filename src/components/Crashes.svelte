0579972b31cffe8a64c9c412383dfff0b12ee66a
MarlonPlease <44271128+MarlonPlease@users.noreply.github.com>
2024-03-13 23:21:10 -0700

    Y-axis

--- src/components/Crashes.svelte
+++ src/components/Crashes.svelte
@@ -60,7 +60,7 @@ function updateDataAndChart() {
 
     // Y scale (count of incidents)
     const y = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.count)])
+      .domain([0, 15])
       .nice()
       .range([height, 0]);
 
