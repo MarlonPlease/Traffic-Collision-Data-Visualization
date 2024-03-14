64597b30d8dccc6e2774a4e2e55a2dd77630477b
MarlonPlease <44271128+MarlonPlease@users.noreply.github.com>
2024-03-13 23:16:26 -0700

    x-axis

--- src/components/Crashes.svelte
+++ src/components/Crashes.svelte
@@ -80,7 +80,7 @@ function updateDataAndChart() {
     // Append x-axis
     svg.append("g")
       .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%d")));
 
     // Append y-axis
     svg.append("g")
