ab3eef773a3b29f7023b1ba4a569e3cea62099fb
MarlonPlease <44271128+MarlonPlease@users.noreply.github.com>
2024-03-13 21:44:40 -0700

    Updated Months

--- src/components/Crashes.svelte
+++ src/components/Crashes.svelte
@@ -120,6 +120,16 @@ function updateDataAndChart() {
 <select bind:value={selectedMonth} on:change={updateMonth}>
   <option value="0">January</option>
   <option value="1">February</option>
+  <option value="2">March</option>
+  <option value="3">April</option>
+  <option value="4">May</option>
+  <option value="5">June</option>
+  <option value="6">July</option>
+  <option value="7">August</option>
+  <option value="8">September</option>
+  <option value="9">October</option>
+  <option value="10">November</option>
+  <option value="11">December</option>
   <!-- ... other months ... -->
 </select>
 
